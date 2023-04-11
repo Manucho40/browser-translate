f126d496df2a74bade1b267f1ccacc1cfa1b7882
Manucho40 <rickypine40@gmail.com>
2023-04-11 17:06:51 +0000

    appel api translate

--- src/chromeServices/DOMEvaluator.ts
+++ src/chromeServices/DOMEvaluator.ts
@@ -8,26 +8,36 @@
         {
           target: { tabId: currentTab.id || 0 },
           func: () => {
-            document.addEventListener("mouseup", function() {
+            document.addEventListener("mouseup", function(e) {
             // Récupérer l'objet de sélection
             const selection: Selection | null = window.getSelection();
-      
+            var tooltip = document.createElement("div");
+            let X: number = 0
+            let Y: number = 0
             // Vérifier que le texte est sélectionné
             if (selection !== null && selection.toString().length > 0) {
             // Récupérer le texte sélectionné
             const selectedText: string = selection.toString();
             // console.log("Le texte sélectionné est : " + selectedText);
-            const tooltip = document.createElement("div");
+              X = e.clientX + window.pageXOffset
+              Y = e.clientY + window.pageYOffset
+            tooltip.classList.add("tooltip"); 
             tooltip.innerHTML = "Vous avez sélectionné : " + selectedText;
             tooltip.style.position = "absolute";
-            tooltip.style.top = selection.getRangeAt(0).getBoundingClientRect().bottom + "px";
-            tooltip.style.left = selection.getRangeAt(0).getBoundingClientRect().left + "px";
+            tooltip.style.top = Y + "px";
+            tooltip.style.left = X + "px";
             tooltip.style.backgroundColor = "lightblue";
             tooltip.style.padding = "5px";
             tooltip.style.borderRadius = "5px";
             document.body.appendChild(tooltip);
+            document.addEventListener("mousedown", function(e) {
+              const tooltip = document.querySelector(".tooltip");
+              if (tooltip !== null) {
+                tooltip.remove();
+              }
+            });
             }
-
+            
             });
             
             
@@ -35,4 +45,12 @@
         }
       );
     });
+
+
+
+    async function translate() {
+      const response = await fetch(`https://api.mymemory.translated.net/get?q=Hello World!&langpair=en|fr`).then(res => {
+        console.log(res)
+      })
+  }
 export {}
